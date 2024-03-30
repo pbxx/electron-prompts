c307352f35c1eb5a0ed7710a5ad19c1cc91d1c29
Donavan Lewis <airlew@gmail.com>
2024-03-30 15:29:53 -0600

    add better readme

--- README.md
+++ README.md
@@ -1,3 +1,59 @@
-# electron-prompts
 
-An easy tool to enable prompts from the Electron main proecss
+# electron-prompts
+![Logo](assets/prompt-screenshot.png)
+
+An easy tool to enable prompts from the Electron main process
+
+# Installation
+```bash
+npm install electron-prompts
+```
+
+# Usage
+Import and instantiate a `PromptManager` for your project:
+```js
+import PromptManager from "../index.js"
+
+const prompts = new PromptManager({
+	devMode: true,
+})
+```
+
+Create and spawn a prompt using *prompt templates*:
+```js
+const pTemplate = {
+	windowTitle: "electron-prompts",
+	cancelButton: {
+		classes: ["btn", "btn-secondary"],
+		// value: "Lets slow'r down there partner..."
+	},
+	elements: [
+		{
+			type: "header",
+			value: "Enter test value",
+		},
+		{
+			type: "paragraph",
+			value: "Add a space to the end of the default value to test changed value:",
+		},
+		{
+			name: "testValue",
+			type: "input",
+			placeholder: "Test input",
+			classes: ["form-control"],
+			value: "Add a space after me please:",
+		},
+	],
+	buttons: [
+		{
+			name: "submit",
+			classes: ["btn", "btn-primary"],
+			value: "Save Changes",
+		},
+	],
+}
+
+const result = await prompts.spawn(pTemplate)
+```
+
+*further documentation coming soon...*
--- test/test.js
+++ test/test.js
@@ -40,10 +40,92 @@ const pTemplate = {
 const result = await prompts.spawn(pTemplate)
 
 it("correct value should be returned on changed prompt", async () => {
-	
-	// expect(results).to.be.an("array").that.is.not.empty
-	// console.log(result)
+	expect(result).to.be.an("object").that.is.not.empty
 	expect(result.values.testValue).to.be.ok
 	expect(result.values.testValue).to.equal("Add a space after me please: ")
+	return
+})
+
+const cancelTemplate = {
+	...pTemplate,
+	elements: [
+		{
+			type: "header",
+			value: "Test Prompt cancellation",
+		},
+		{
+			type: "paragraph",
+			value: "CANCEL this prompt",
+		},
+		{
+			name: "testValue",
+			type: "input",
+			placeholder: "Test input",
+			classes: ["form-control"],
+			value: "You do not want to enter this value and you are pressing cancel...",
+		},
+	]
+}
+
+const cancelTemplate2 = {
+	...pTemplate,
+	elements: [
+		{
+			type: "header",
+			value: "The easiest prompts ever!",
+		},
+		{
+			type: "paragraph",
+			value: "An easy tool to enable spawning prompts from the Electron main process",
+		},
+		{
+			name: "testValue",
+			type: "input",
+			placeholder: "Test input",
+			classes: ["form-control"],
+			value: "With inputs!",
+		},
+		{
+			type: "paragraph",
+			value: "Also paragraphs",
+		},
+		{
+			name: "testValue",
+			type: "input",
+			placeholder: "Or even another input!",
+			classes: ["form-control"],
+			value: "",
+		},
+	]
+}
+
+const result2 = await prompts.spawn(cancelTemplate2)
+
+it("correct value should be returned on cancelled prompt", async () => {
+	expect(result2).to.equal(null)
+	expect(result2).to.not.be.ok
+	return
+})
+
+
+const closedTemplate = {
+	...pTemplate,
+	elements: [
+		{
+			type: "header",
+			value: "Test Prompt closing",
+		},
+		{
+			type: "paragraph",
+			value: "CLOSE this prompt",
+		}
+	]
+}
+
+const result3 = await prompts.spawn(closedTemplate)
+
+it("same value should be returned on a closed prompt", async () => {
+	expect(result3).to.equal(null)
+	expect(result3).to.not.be.ok
 	return
 })
